e5afed3c9905fe3b331a4859da9b366f8190e8b2
Dongrun Jiao <dongrunjiao@DongrundeMacBook-Pro.local>
2025-02-21 15:43:12 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,15 @@ export const metadata: Metadata = {
   title: "前端知识点",
   description: "前端知识点",
 };
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -19,6 +28,7 @@ export default function RootLayout({
         <div className="main">
           <div className="gradient"></div>
         </div>
+        <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}></div>
         <main className="app">
           <SlideBar />
           <div className="w-full h-full">{children}</div>
